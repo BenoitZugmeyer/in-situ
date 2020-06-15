974fd978cce268fcf65cdf2fef8933037211047d
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2020-06-15 19:42:17 +0200

    style: move tests around

--- tests/bin.test.js
+++ tests/bin.test.js
@@ -1,6 +1,80 @@
 const { spawn } = require("child_process")
 const http = require("http")
 
+// generated with
+// npx terser index.js --mangle -o bundle.min.js -e --toplevel --source-map includeSources
+const generatedCode =
+  "(function(){const o=document.title;console.log(`\t`,o);window.杨=o})();"
+
+const sourceMap = JSON.stringify({
+  version: 3,
+  file: "bundle.min.js",
+  sources: ["index.js"],
+  names: ["title", "document", "console", "log", "window", "杨"],
+  mappings: "YAAA,MAAMA,EAAQC,SAASD,MACvBE,QAAQC,IAAI,KAAKH,GACjBI,OAAOC,EAAIL",
+  sourcesContent: [
+    "const title = document.title\nconsole.log(`\t`, title)\nwindow.杨 = title\n",
+  ],
+})
+
+test("fails if no argument is given", async () => {
+  expect(await runBin()).toMatchSnapshot()
+})
+
+test("beautifies context", async () => {
+  const url = await withServer({
+    "/": "if(i)j.k",
+  })
+  expect(await runBin(`${url}:1:8`)).toMatchSnapshot()
+})
+
+describe("source map retrieval", () => {
+  async function testSourceMapRetrieval(responses) {
+    const url = await withServer(responses)
+    expect(await runBin(`${url}/bundle.min.js:1:66`)).toMatchSnapshot()
+  }
+
+  test("use the source map from a sourcemap comment", async () => {
+    await testSourceMapRetrieval({
+      "/bundle.min.js": `${generatedCode}\n//# sourceMappingURL=bundle.min.js.map`,
+      "/bundle.min.js.map": sourceMap,
+    })
+  })
+
+  test("use the source map from a X-SourceMap header", async () => {
+    await testSourceMapRetrieval({
+      "/bundle.min.js": {
+        body: generatedCode,
+        headers: { "X-SourceMap": "bundle.min.js.map" },
+      },
+      "/bundle.min.js.map": sourceMap,
+    })
+  })
+
+  test("use the source map from a SourceMap header", async () => {
+    await testSourceMapRetrieval({
+      "/bundle.min.js": {
+        body: generatedCode,
+        headers: { SourceMap: "bundle.min.js.map" },
+      },
+      "/bundle.min.js.map": sourceMap,
+    })
+  })
+
+  test("use the source map from a data-uri", async () => {
+    const base64EncodedSourceMap = Buffer.from(sourceMap).toString("base64")
+    await testSourceMapRetrieval({
+      "/bundle.min.js": `${generatedCode}\n//@ sourceMappingURL=data:application/json;charset=utf-8;base64,${base64EncodedSourceMap}`,
+    })
+  })
+
+  test("fallback to beautify if the source map is not found", async () => {
+    await testSourceMapRetrieval({
+      "/bundle.min.js": `${generatedCode}\n//# sourceMappingURL=bundle.min.js.map`,
+    })
+  })
+})
+
 function runBin(...args) {
   return new Promise((resolve) => {
     const path = require.resolve("../src/main")
@@ -51,76 +125,3 @@ afterEach(() => {
   servers.length = 0
 })
 
-test("fails if no argument is given", async () => {
-  expect(await runBin()).toMatchSnapshot()
-})
-
-test("beautifies context", async () => {
-  const url = await withServer({
-    "/": "if(i)j.k",
-  })
-  expect(await runBin(`${url}:1:8`)).toMatchSnapshot()
-})
-
-describe("source map", () => {
-  // generated with
-  // npx terser index.js --mangle -o bundle.min.js -e --toplevel --source-map includeSources
-  const generatedCode =
-    "(function(){const o=document.title;console.log(`\t`,o);window.杨=o})();"
-  const sourceMap = JSON.stringify({
-    version: 3,
-    file: "bundle.min.js",
-    sources: ["index.js"],
-    names: ["title", "document", "console", "log", "window", "杨"],
-    mappings:
-      "YAAA,MAAMA,EAAQC,SAASD,MACvBE,QAAQC,IAAI,KAAKH,GACjBI,OAAOC,EAAIL",
-    sourcesContent: [
-      "const title = document.title\nconsole.log(`\t`, title)\nwindow.杨 = title\n",
-    ],
-  })
-
-  async function testSourceMapRetrieval(responses) {
-    const url = await withServer(responses)
-    expect(await runBin(`${url}/bundle.min.js:1:66`)).toMatchSnapshot()
-  }
-
-  test("use the source map from a sourcemap comment", async () => {
-    await testSourceMapRetrieval({
-      "/bundle.min.js": `${generatedCode}\n//# sourceMappingURL=bundle.min.js.map`,
-      "/bundle.min.js.map": sourceMap,
-    })
-  })
-
-  test("use the source map from a X-SourceMap header", async () => {
-    await testSourceMapRetrieval({
-      "/bundle.min.js": {
-        body: generatedCode,
-        headers: { "X-SourceMap": "bundle.min.js.map" },
-      },
-      "/bundle.min.js.map": sourceMap,
-    })
-  })
-
-  test("use the source map from a SourceMap header", async () => {
-    await testSourceMapRetrieval({
-      "/bundle.min.js": {
-        body: generatedCode,
-        headers: { SourceMap: "bundle.min.js.map" },
-      },
-      "/bundle.min.js.map": sourceMap,
-    })
-  })
-
-  test("use the source map from a data-uri", async () => {
-    const base64EncodedSourceMap = Buffer.from(sourceMap).toString("base64")
-    await testSourceMapRetrieval({
-      "/bundle.min.js": `${generatedCode}\n//@ sourceMappingURL=data:application/json;charset=utf-8;base64,${base64EncodedSourceMap}`,
-    })
-  })
-
-  test("fallback to beautify if the source map is not found", async () => {
-    await testSourceMapRetrieval({
-      "/bundle.min.js": `${generatedCode}\n//# sourceMappingURL=bundle.min.js.map`,
-    })
-  })
-})
